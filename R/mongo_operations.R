3223dbba4a8ff13f71fd2b9d782e237c624d8e0e
Mariya Dimitrova <mry.dmtrv@gmail.com>
2019-06-03 16:12:00 +0300

    fns to query mongo collecton and save within it

---
+++ R/mongo_operations.R
@@ -0,0 +1,34 @@
+
+#' @export
+check_mongo = function(value, type, collection, regex)
+{
+  if (regex == TRUE){
+    query = sprintf("{\"%s\":{\"%s\":\"%s\",\"%s\":\"%s\"}}", "value", "$regex", value, "$options", "i")
+  }else{
+    query = sprintf("{\"%s\":\"%s\",\"%s\":\"%s\"}", "value", value, "type", type)
+    print(query)
+  }
+  key = collection$find(query)$key
+  return(key)    
+}
+
+#' @export
+check_mongo_via_orcid = function(orcid, collection)
+{
+  query = sprintf("{\"%s\":{\"%s\":\"%s\",\"%s\":\"%s\"}}", "key", "$regex", orcid, "$options", "i")
+  collection$find(query)
+  key = collection$find(query)$key
+  return(key)    
+}
+
+#' @export
+save_to_mongo = function(key, value, type, collection)
+{
+  d = data.frame(
+    key = as.character(key),
+    value = as.character(value),
+    type = as.character(type)
+  )
+  collection$insert(d)
+}
+
