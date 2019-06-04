e843b96e2739124a51194d3b945861e6b6151e50
mariyad <43512677+mariyad@users.noreply.github.com>
2019-06-04 13:22:06 +0300

    curlies

--- R/identifier_new.R
+++ R/identifier_new.R
@@ -15,7 +15,8 @@ identifier_new = function (node, xml, schema_name, mongo_key, prefix = NA, blank
       component_df =  process_schema_component(node, mongo_key)
       id = get_or_set_mongoid(component_df, schema_name, mongo_key)
       xml2::xml_attr(node, "obkms_id") = id
-    }} else{
+    }
+  } else{
       #if there is an obkms_id, just extract it
       id = xml2::xml_attr(node, "obkms_id")
     }
