f87b8f0e30506ce2ee18b1ba8ebc622a03b05ee4
Mariya Dimitrova <mry.dmtrv@gmail.com>
2019-06-17 10:15:01 +0300

    Added back bold process/record and included genbank ids

--- R/process_bold_records.R
+++ R/process_bold_records.R
@@ -1,55 +1,98 @@
+
 #' @export
-process_bold <- function(file)
+process_bold_genbank <- function(file)
 {
-  suppressWarnings({
-    xml <- xml2::read_xml(file)
-    #xpath to find all bold systems links within article!
-    results <- xml2::xml_find_all(xml, "//*[starts-with(@xlink:href, 'http://www.boldsystems.org/')] | //*[starts-with(@xlink:href, 'http://boldsystems.org/')]")
-    if(!(length(results)==0))
-    {
-      doc <- XML::xmlParse(xml, isHTML = FALSE)
-      nodeset <- XML::getNodeSet(doc, "//article-meta")
-      #creates parent nodes for the bold-ids or bins section
-      parent_id <- XML::newXMLNode("bold-ids", parent = nodeset)
-      empty_bold_id <- TRUE
-      for (r in results)
+    suppressWarnings({
+      xml = xml2::read_xml(file)
+
+      doc = XML::xmlParse(xml, isHTML = FALSE)
+      nodeset = XML::getNodeSet(doc, "//article-meta")
+      #xpath to find all bold systems links within article!
+      bold_results = xml2::xml_find_all(xml, "//*[starts-with(@xlink:href, 'http://www.boldsystems.org/')] | //*[starts-with(@xlink:href, 'http://boldsystems.org/')]")
+      if(!(length(bold_results)==0))
       {
-        node_string <- sub('.*http', '', r)
-        #handles cases where the node value is either a link or just an id
-        #if we have 2 closing tags => the value is just an id
-        #1 closing tag means the value contained a link and sub('.*http','', r) matched and replaced the second 'http' (within the value)
-        if (stringi::stri_count_regex(node_string, '>') > 1){
-          string <- sub('\">?.*', '', node_string)
-        }
-        else {
-          string <- sub('<.*', '', node_string)
-        }
-
-        #adds the http part to url_parse the string => break it down into parts and only take the query part
-        string <- paste0("http", string)
-        result <- xml2::url_parse(string)
-        query <- result$query
-
-        empty_bold_id = FALSE
-        tag = "bold-id"
-        new_node <- XML::newXMLNode(tag, parent = parent_id)
-
-        value <- sub('.*=', '', query)
-        XML::xmlValue(new_node) <- value
+        nodeset = bold_xml_modifier(nodeset = nodeset, results = bold_results)
       }
 
-      if (empty_bold_id == TRUE)
+      genbank_results = xml2::xml_find_all(xml, "//ext-link[@ext-link-type='gen']")
+      if(!(length(genbank_results)==0))
       {
-        XML::removeNodes(parent_id)
+        nodeset = genbank_xml_modifier(nodeset = nodeset, results = genbank_results)
       }
 
-      #saving_file_name <- paste0(dirname(file), "/modified/",basename(file), "_mod.xml" )
-      #saveXML(doc, saving_file_name)
       new_xml = XML::saveXML(doc)
+
+      return(new_xml)
+    })
+  }
+
+
+#' @export
+bold_xml_modifier = function(nodeset, results){
+
+  #creates parent nodes for the bold-ids or bins section
+  parent_id = XML::newXMLNode("bold-ids", parent = nodeset)
+  parent_bin = XML::newXMLNode("bins", parent = nodeset)
+  empty_bin = TRUE
+  empty_bold_id = TRUE
+  for (r in results)
+  {
+    node_string = sub('.*http', '', r)
+    #handles cases where the node value is either a link or just an id
+    #if we have 2 closing tags => the value is just an id
+    #1 closing tag means the value contained a link and sub('.*http','', r) matched and replaced the second 'http' (within the value)
+    if (stringi::stri_count_regex(node_string, '>') > 1){
+      string = sub('\">?.*', '', node_string)
     }
-    else{
-      new_xml = NULL
+    else {
+      string = sub('<.*', '', node_string)
     }
-    return(new_xml)
-  })
+
+    #adds the http part to url_parse the string => break it down into parts and only take the query part
+    string <- paste0("http", string)
+    result <- xml2::url_parse(string)
+    query <- result$query
+
+    #if the query part contains "bin" or "clusteruri" the uri contains a bin
+    #else - bold-id (which can be anything, like record-id or process-id)
+    if((sub('=.*', '', query) == "bin") || (sub('=.*', '', query) == "clusteruri" ))
+    {
+      empty_bin = FALSE
+      tag = "bin"
+      new_node = XML::newXMLNode(tag, parent = parent_bin)
+
+    } else
+    {
+      empty_bold_id = FALSE
+      tag = "bold-id"
+      new_node = XML::newXMLNode(tag, parent = parent_id)
+    }
+    value = sub('.*=', '', query)
+    XML::xmlValue(new_node) = value
+  }
+  #remove the empty nodesets
+  if (empty_bin == TRUE)
+  {
+    XML::removeNodes(parent_bin)
+  }
+  if (empty_bold_id == TRUE)
+  {
+    XML::removeNodes(parent_id)
+  }
+
+  return(nodeset)
+}
+
+#' @export
+genbank_xml_modifier = function(nodeset, results){
+  #creates parent nodes for the bold-ids or bins section
+  parent_gen = XML::newXMLNode("genbank-ids", parent = nodeset)
+  for (r in results)
+  {
+    node_string = xml2::xml_text(r)
+    tag = "genbank-id"
+    new_node = XML::newXMLNode(tag, parent = parent_gen)
+    XML::xmlValue(new_node) = node_string
+  }
+  return(nodeset)
 }
