7f3a3109e44275720148b50f1e9f5be9c0689803
mariyad <43512677+mariyad@users.noreply.github.com>
2019-07-25 14:23:21 +0300

    Update identifier_new.R

--- R/identifier_new.R
+++ R/identifier_new.R
@@ -8,18 +8,17 @@ identifier_new = function (node, xml, mongo_key, prefix = NA, blank = FALSE)
   if (blank == TRUE) {
     prefix = c(`_` = "_")
   }
+  
+  
   if (is.na(xml2::xml_attr(node, "obkms_id"))) {
-    if (!is.na(mongo_key)) {
+    
       component_df = process_schema_component(node, mongo_key)
       id = get_or_set_mongoid(component_df, prefix)
       xml2::xml_attr(node, "obkms_id") = id
-      xml2::xml_attr(node, "obkms_prefix") = prefix
-    }
-  }
-  else {
+    }else {
     id = xml2::xml_attr(node, "obkms_id")
   }
-
+  
   if(is.null(id))
     ident = NULL
   else
