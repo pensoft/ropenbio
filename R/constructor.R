9330b829231d28e9d713c1a96c565a15c36f514e
mariyad <43512677+mariyad@users.noreply.github.com>
2019-08-06 11:44:56 +0300

    Update constructor.R

--- R/constructor.R
+++ R/constructor.R
@@ -456,9 +456,11 @@ treatment = function (atoms, identifiers, prefix, schema_name = xml_schema$schem
   tt$add_triple(treatment_id, is_contained_by, identifiers$pid)
   if (length(atoms$status)>0 ){
     status = atoms$status[[1]]$text_value
-    tt$add_triple(treatment_id, taxonStatus, literal(status))
-    if (!(is.null(status)) & status %in% new_taxons ==TRUE)
+    if (!(is.null(status)){
+      tt$add_triple(treatment_id, taxonStatus, literal(status))
+     if (status %in% new_taxons ==TRUE)
       tt$add_triple(treatment_id, mentions, TaxonomicDiscovery)
+      }
   }
   #for (a in 1:length(atoms$text_content)){
   #  atoms$text_content[[a]]$text_value = escape_special(atoms$text_content[[a]]$text_value)
