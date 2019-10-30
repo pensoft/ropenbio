3ad549921cfc58ea7e6b8b4abab2236e38ecc200
Mariya Dimitrova <mry.dmtrv@gmail.com>
2019-10-30 17:28:00 +0200

    fix

--- R/constructor.R
+++ R/constructor.R
@@ -559,7 +559,7 @@ bibliography = function (atoms, identifiers, prefix, new_taxons, mongo_key)
 
 
     article_doi = unlist(atoms$doi[1])["text_value"]
-    article_title = unlist(article_title[1])["text_value"]
+    article_title = unlist(atoms$article_title[1])["text_value"]
 
     key = check_mongo_citation(value = article_title, parent = article_doi, collection = general_collection)
     df = set_component_frame(label = article_title, mongo_key = NA, type = "article", orcid = NA, parent = article_doi, key = NA)
