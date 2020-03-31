36834aaa10becac92bd08a8efa6e090b6ef700f2
Mariya Dimitrova <mry.dmtrv@gmail.com>
2020-03-31 17:46:22 +0300

    modified extractor

--- R/extractors.R
+++ R/extractors.R
@@ -57,57 +57,53 @@ node_extractor = function (node, xml_schema, reprocess, triples, prefix, new_tax
 #' @export
 node_extractor_en = function (node, xml_schema, reprocess, triples, prefix, new_taxons,
           dry = FALSE, filename, root_id, publisher_id = publisher_id,
-          journal_id = journal_id)
+          journal_id = journal_id, plazi_doc = plazi_doc, plazi_treatment_id=plazi_treatment_id)
 {
+  print(node)
+  
+#  print(!is.null(xml_schema$injector))
   if (processing_status(node) == FALSE || reprocess == TRUE && !(is.null(triples)) || xml2::xml_name(node)=="article") {
-    if (!is.null(xml_schema$injector)) {
-      xml_schema$injector(obkms_id = rdf4r::last_token(rdf4r::strip_filename_extension(filename),
-                                                       split = "/"), node)
-    }
+  #  if (!is.null(xml_schema$injector)) {
+  #    xml_schema$injector(obkms_id = rdf4r::last_token(rdf4r::strip_filename_extension(filename),
+   #                                                    split = "/"), node)
+  #  }s
 
-    tic("find literals")
     atoms = find_literals(node, xml_schema)
+    print(atoms)
     #TODO fix parent_id prefix
-    toc(log = TRUE)
-
-    mgk = xml2::xml_name(node)
-    message = paste("constructor + id new: ", mgk)
-    tic(message)
+    print(xml_schema$schema_name)
+    if (plazi_doc == TRUE && xml_schema$schema_name %in% c("nomenclature_section", "materials_examined", "diagnosis_section", "distribution_section", "discussion_section", "taxonomic_key", "figure", "taxonomic_name_usage", "reference")){
+      pid = plazi_treatment_id
+    }else{
+      pid = identifier(parent_id(node), prefix)
+    }
+    print(pid)
     new_triples = xml_schema$constructor(atoms, identifiers = list(nid = identifier_new(node, xml, mongo_key = xml_schema$mongo_key,prefix = prefix, blank = FALSE, publisher_id =publisher_id, journal_id=journal_id),
-                                                                   pid = identifier(parent_id(node), prefix),
+                                                                   pid = pid,
                                                                    root_id = root_id),
                                          prefix = xml_schema$prefix,new_taxons = new_taxons, mongo_key = xml_schema$mongo_key, publisher_id = publisher_id,
-                                         journal_id = journal_id)
-    toc(log = TRUE)
-    tic(paste("set context: ", mgk))
+                                         journal_id = journal_id, plazi_doc = plazi_doc)
 
     new_triples$set_context(triples$context)
-    toc(log = TRUE)
 
   #  serialization = new_triples$serialize()
     # if (dry == FALSE) {
     #    add_data(serialization, access_options = access_options)
     #  }
 
-    tic("set obkms_process = TRUE")
     xml2::xml_attr(node, "obkms_process") = "TRUE"
-    toc(log = TRUE)
 
-    tic("add new_triples to triples")
     triples$add_triples(new_triples)
-    toc(log = TRUE)
-
 
   }
   for (c in xml_schema$components) {
     nodel = xml2::xml_find_all(node, c$xpath)
     for (n in nodel) {
-
-      node_extractor_en(n, c, reprocess = reprocess, triples = triples,
+      
+    node_extractor_en(n, c, reprocess = reprocess, triples = triples,
                         prefix = prefix, new_taxons = new_taxons, dry = dry, filename = filename,
                         root_id = root_id, publisher_id = publisher_id,
-                        journal_id = journal_id)
-
+                        journal_id = journal_id, plazi_doc = plazi_doc, plazi_treatment_id = plazi_treatment_id)
 
     }
   }
